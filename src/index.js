4bec35cc661ab403c86abbf90204b645eb60c9da
tekacode <81430335+tekacode@users.noreply.github.com>
2021-05-27 08:11:36 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//1. Print the 1 index sentence from the array.
+
+let myArray = [
+  "Summer is around the corner",
+  "Winter is very long is NE of the US",
+  "JavaScript is very fun"
+];
+
+console.log(myArray[1]);
+// 2. Create a for loop that starts at zero and ends at 20, counting by twos.
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+
